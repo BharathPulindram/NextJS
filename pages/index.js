53227ea1d7e8a4da9b652ddbc4c4195a49a48bf7
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-02 21:09:30 -0600

    link navigation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import About from "./about";
+import Link from "next/link";
 function Home() {
   return (
     <>
       <h1>Home Page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Product</Link>
     </>
   );
 }
