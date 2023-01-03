43bf56511088f21989fb54e6c603dab9a8054ef3
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-02 21:12:38 -0600

    dynamic navigation for product

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
 function ProductList() {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/">Home</Link>
+      <Link href="/product/1">
+        <h2>Product 1</h2>
+      </Link>
+      <Link href="/product/2">
+        <h2>Product 2</h2>
+      </Link>
+      <Link href="/product/3">
+        <h2>Product 3</h2>
+      </Link>
     </>
   );
 }
