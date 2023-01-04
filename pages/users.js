48f49d263bd2cef7c619c653ff1818af001c75fa
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-04 15:49:51 -0600

    rendered list of users in userlist component

--- pages/users.js
+++ pages/users.js
@@ -1,5 +1,16 @@
 function UserList({ users }) {
-  return <h1>List of users</h1>;
+  console.log(users);
+  return (
+    <>
+      <h1>Users :</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default UserList;
