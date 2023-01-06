9b10a17d7e0cd9c65f98d18753673bd9a52dd27b
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-06 16:29:08 -0600

    Styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 //Global styles should be include in this file
-import '../styles/globals.css'
+import '../styles/globals.css';
+import {ThemeProvider} from 'styled-components';
+
+const theme = {
+  colors:{
+    primary:'#55C7D'
+  }
+}
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+    </ThemeProvider>
 }
--- pages/css-in-js.js
+++ pages/css-in-js.js
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
 function CSSJS() {
-  return <h2 style={{ color: "red" }}>Hello CSS JS</h2>;
+  return <Title>Styled Component</Title>;
 }
 
 export default CSSJS;
