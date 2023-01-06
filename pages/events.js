17af9080fd2fbafa5125c54347733aeb59e463a9
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-06 13:33:58 -0600

    pre rendering + client data fetching with shallow routing

--- pages/events.js
+++ pages/events.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 function EventList({ eventList }) {
+  const router = useRouter();
   const [events, setEvent] = useState(eventList);
   const fetchSportsEvents = async () => {
     const response = await fetch(`http://localhost:3000/api/articles`);
     const data = await response.json();
     const filter = data.filter((article) => article.category === "sports");
     setEvent(filter);
+    router.push("/events?category=sports", undefined, { shallow: true });
   };
   return (
     <>
@@ -28,8 +31,13 @@ function EventList({ eventList }) {
 
 export default EventList;
 
-export async function getServerSideProps() {
-  const response = await fetch(`http://localhost:3000/api/articles`);
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { category } = query;
+  const queryString = category ? "category=sports" : "";
+  const response = await fetch(
+    `http://localhost:3000/api/articles?${queryString}`
+  );
   const data = await response.json();
 
   return {
