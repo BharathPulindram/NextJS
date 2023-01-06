8317022b42fcd3d88c2bf24894ce20a870574c63
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-06 16:20:49 -0600

    added scss styles into page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
-import styles from "../styles/about.module.css";
+//import styles from "../styles/about.module.css";
+import styles from "../styles/About.module.scss";
 function About() {
-  return <h1 className={styles.highlight}>About Page!!</h1>;
+  return <h1 className={styles.highLightscss}>About Page!!</h1>;
 }
 
 export default About;
