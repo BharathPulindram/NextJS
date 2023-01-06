23fa71904db5db77f15d48e94a1d9dd74e134707
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-06 16:16:14 -0600

    global and component level styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-//import '../styles/globals.css'
+//Global styles should be include in this file
+import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
+import styles from "../styles/about.module.css";
 function About() {
-  return <h1>About Page!!</h1>;
+  return <h1 className={styles.highlight}>About Page!!</h1>;
 }
 
 export default About;
