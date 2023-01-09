5fb5b2791f1396405348818f94503651c5a056cc
Bharath Pulindram <bharath.pulindram@gmail.com>
2023-01-09 11:12:18 -0600

    replaced img with Image tag from next

--- pages/images.js
+++ pages/images.js
@@ -1,10 +1,13 @@
+import Image from "next/image";
+import img from "../public/image1.jpg";
 function images() {
   const imageArr = ["image1", "image2", "image3", "image4", "image5"];
   return (
     <>
+      <Image src={img} alt="img" placeholder="blur" width="280" height="420" />
       {imageArr.map((path) => (
         <div key={path}>
-          <img src={`/${path}.jpg`} alt="pic" width="280" height="420"></img>
+          <Image src={`/${path}.jpg`} alt="pic" width="280" height="420" />
         </div>
       ))}
     </>
